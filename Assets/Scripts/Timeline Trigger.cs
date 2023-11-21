ca2353dc6b96b3ce3c8cf180ba45d34c23a02aa6
Cameron Traer <crtraer@gmail.com>
2023-11-21 11:45:48 -0400

    Updated Script to play/stop with debug log

--- Assets/Scripts/Timeline Trigger.cs
+++ Assets/Scripts/Timeline Trigger.cs
@@ -8,10 +8,21 @@ public class TimelineTrigger : MonoBehaviour
     public PlayableDirector Timeline_Event; //Name of the TimeLine slot for the script, visuble in Unity    
 
     // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other) //Changed from Void Start (Doesn't start on play)
+    
+    
     {
         Timeline_Event.Play();   //Reference the "Timeline_Event" in here with ".play();" at the end. 
+        Debug.Log("Player stepped in trigger bounds. The Timeline has be started.");
     }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        Timeline_Event.Stop(); //Stops the timeline from playing when the player steps off the platform
+        Debug.Log("Player stepped out of trigger bounds. The Timeline has been stopped");
+    }
+
 
     // Update is called once per frame
     void Update()
