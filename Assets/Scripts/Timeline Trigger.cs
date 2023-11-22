3bc86f8a444a36e304a82c7096a531df9bd7c075
NitroTech 4K <crtraer@gmail.com>
2023-11-22 16:57:57 -0400

    Finished Water Animations in TimeLine (Needs Sounds)

--- Assets/Scripts/Timeline Trigger.cs
+++ Assets/Scripts/Timeline Trigger.cs
@@ -4,26 +4,44 @@ using UnityEngine;
 using UnityEngine.Playables; //Add this at the start to gain functions for the timeline
 public class TimelineTrigger : MonoBehaviour
 {
+   
+    public PlayableDirector Entering_Timeline; //Name of the TimeLine slot for the script, visuble in Unity
+    public PlayableDirector Exiting_Timeline;                                        //
+    public GameObject ObjectToDeactivate1;
 
-    public PlayableDirector Timeline_Event; //Name of the TimeLine slot for the script, visuble in Unity
-                                            //
-    public GameObject ObjectToDeactivate;
+
+    private int TimeLinePlayValue;
+
+    private void Start()
+    {
+        
+    }
 
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other) //Changed from Void Start (Doesn't start on play)
     
-    
+       
+
     {
-        Timeline_Event.Play();   //Reference the "Timeline_Event" in here with ".play();" at the end. 
-        Debug.Log("Player stepped in trigger bounds. The Timeline has be started.");
+            Exiting_Timeline.Stop();
+            Entering_Timeline.Play();   //Reference the "Timeline_Event" in here with ".play();" at the end. 
+            Debug.Log("Player stepped in trigger bounds. The Timeline has be started.");
+        
     }
 
 
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Timeline_Event.Stop(); //Stops the timeline from playing when the player steps off the platform
+    //    Debug.Log("Player stepped out of trigger bounds. The Timeline has been stopped");
+    //    ObjectToDeactivate1.SetActive(false);
+    //}
+
     private void OnTriggerExit(Collider other)
-    {
-        Timeline_Event.Stop(); //Stops the timeline from playing when the player steps off the platform
-        Debug.Log("Player stepped out of trigger bounds. The Timeline has been stopped");
-        ObjectToDeactivate.SetActive(false);
+    { 
+            Entering_Timeline.Stop();
+            Exiting_Timeline.Play();
+            Debug.Log("Player stepped out of trigger bounds. The Exit Timeline will now play");
     }
 
 
